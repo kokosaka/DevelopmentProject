d8e062b2f1532eea6e3553fa13f755bf020fef45
Koboh Kosaka <kobohkosaka@outlook.com>
2021-03-25 14:15:30 -0700

    Add resuable components

--- blood-and-bone/src/baseAtt.js
+++ blood-and-bone/src/baseAtt.js
@@ -1,14 +1,14 @@
+import Attribute from "./attribute";
+
 export default function BaseAttributes(props) {
   return (
     <div>
       Base Attributes
       <div>
-        <ul>
-          <li>Strength <input type="number" className="baseAtt" id="stre" value={props.baseAtt.stre} onChange={props.handleChange}></input></li>
-          <li>Dexterity <input type="number" className="baseAtt" id="dext" value={props.baseAtt.dext} onChange={props.handleChange}></input></li>
-          <li>Mind <input type="number" className="baseAtt" id="mind" value={props.baseAtt.mind} onChange={props.handleChange}></input></li>
-          <li>Presence <input type="number" className="baseAtt" id="pres" value={props.baseAtt.pres} onChange={props.handleChange}></input></li>
-        </ul>
+        {Object.entries(props.baseAtt).map(([att, value]) => {
+            return <Attribute className="baseAtt" att={att} value={value} handleChange={props.handleChange}/>
+          })
+        }
       </div>
     </div>
   );
